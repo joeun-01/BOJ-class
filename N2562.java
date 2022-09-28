3adaf936e51825c8270584eda8c613519abd6e2c
joeun-01 <jjy020121@gmail.com>
2022-09-28 18:05:48 +0900

    22.09.28

--- N2562.java
+++ N2562.java
@@ -8,25 +8,28 @@ public class N2562 {
     public static void main(String[] main) {
         Scanner input = new Scanner(System.in);
 
-        int i;
-        int[] origin = new int[9];
-        ArrayList<Integer> copy = new ArrayList<>();
-
-        for(i = 0; i < 9; i++) {
-            origin[i] = input.nextInt();
-            copy.add(origin[i]);
-        }
-
-        Collections.sort(copy);
-
-        System.out.println(copy.get(8));
-
-        for(i = 0; i < 9; i++) {
-            if(copy.get(8) == origin[i]) {
-                System.out.println(i);
+        try {
+            int i;
+            int[] origin = new int[9];
+            ArrayList<Integer> copy = new ArrayList<>();
+    
+            for(i = 0; i < 9; i++) {
+                origin[i] = input.nextInt();
+                copy.add(origin[i]);
             }
+    
+            Collections.sort(copy);
+    
+            System.out.println(copy.get(8));
+    
+            for(i = 0; i < 9; i++) {
+                if(copy.get(8) == origin[i]) {
+                    System.out.println(i + 1);
+                }
+            }
+        } finally {
+            input.close();
         }
-
     }
     
 }
