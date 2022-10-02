89b5c0e09e0703307b7c6be94e7cf4ab982f76e1
joeun-01 <jjy020121@gmail.com>
2022-10-02 23:55:33 +0900

    22.10.02

--- N2884.java
+++ N2884.java
@@ -6,21 +6,28 @@ public class N2884 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int hour = input.nextInt();
-        int minute = input.nextInt();
-
-        if(minute < 45) {  // 45분 미만인 경우
-            if(hour != 0) {  // 0시 이상인 경우
-                hour -= 1;
-                minute += 15;
-            } else {
-                hour = 23;
-                minute += 15;
+        try {
+            int hour = input.nextInt();
+            int minute = input.nextInt();
+    
+            if(minute < 45) {  // 45분 미만인 경우
+                if(hour != 0) {  // 0시 이상인 경우
+                    hour -= 1;
+                    minute += 15;
+                } else {
+                    hour = 23;
+                    minute += 15;
+                }
+            } else {  // 45분 이상인 경우
+                minute -= 45;
             }
-        } else {  // 45분 이상인 경우
-            minute -= 45;
+
+            System.out.println(hour + " " + minute);
+        } finally {
+            input.close();
         }
 
-        System.out.println(hour + " " + minute);
+
+
     }
 }
