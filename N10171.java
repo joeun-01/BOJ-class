c45f0a088639b36532633ea441bdbbf154799675
joeun-01 <jjy020121@gmail.com>
2022-10-05 13:13:07 +0900

    22.10.05

---
+++ N10171.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class N10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
