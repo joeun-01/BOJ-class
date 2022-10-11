010c8db9618a47861a3735f24bd10bf5d2f3af9d
joeun-01 <jjy020121@gmail.com>
2022-10-11 19:45:09 +0900

    22.10.11

---
+++ N10998.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class N10998 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a * b);
+
+        input.close();
+    }
+}
