0ea3fce81cb8c492959edf417cdc9ef1e23330f9
joeun-01 <jjy020121@gmail.com>
2023-01-20 06:56:00 +0900

    23.01.20

---
+++ N25083.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+/*
+아래 예제와 같이 새싹을 출력하시오.
+
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+ */
+
+public class N25083 {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" + 	
+        "r`-_   ,'  ,/\n" + 	
+        " \\. \". L_r'\n" + 		
+        "   `~\\/\n" + 			
+        "      |\n" + 		
+        "      |";
+
+        System.out.print(s);    
+    }
+}
