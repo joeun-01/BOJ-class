a42e1ff0391d06906baaabe132a73b919b5d19ca
joeun-01 <jjy020121@gmail.com>
2023-01-24 18:13:16 +0900

    23.01.24

---
+++ N11382.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class N11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        
+        System.out.println(a + b + c);
+
+        scanner.close();
+    }
+}
