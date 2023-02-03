2320017cc39948c095985f6c231d4e30767d3d17
joeun-01 <jjy020121@gmail.com>
2023-02-03 13:03:15 +0900

    23.02.03

--- N1043.java
+++ N1043.java
@@ -38,17 +38,94 @@ N, M은 50 이하의 자연수이고, 진실을 아는 사람의 수는 0 이상
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class N1043 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+    static int[] parent;
 
-        StringTokenizer num = new StringTokenizer(read.readLine());
-        int member = Integer.parseInt(num.nextToken());
-        int party = Integer.parseInt(num.nextToken());
+    static int n,m;
+    static int ans = 0;
 
+    static boolean know[];
+
+    static ArrayList<Integer>party[];
+    static ArrayList<Integer>people[];
+
+    public static void main(String[] args) throws  IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
+        String [] t = br.readLine().split(" ");
+        n = Integer.parseInt(t[0]);
+        m = Integer.parseInt(t[1]);
+
+        know = new boolean[n+1];
+        people = new ArrayList[n+1];
+        party = new ArrayList[m];
+
+        for(int i=1; i<=n; i++) {
+            people[i] = new ArrayList<>();
+        }
+        for(int i=0; i<m; i++) {
+            party[i] = new ArrayList<>();
+        }
+        
+        t = br.readLine().split(" ");
+        
+        for(int i=1; i<t.length; i++) {
+            know[Integer.parseInt(t[i])] = true;    // 이 사람들은 진실을 알고있다
+        }
+        
+        
+        for(int i=0; i<m; i++) {
+            t = br.readLine().split(" ");
+            for(int j=1; j<t.length; j++) {
+                int tmp = Integer.parseInt(t[j]);
+                party[i].add(tmp);      
+                people[tmp].add(i);
+            }
+        }
+        bfs();
+    }
+    public static void bfs() {
+        Queue<Integer> q= new LinkedList<>();
+        boolean visited [] = new boolean [m];
+        
+        for(int i=1; i<=n; i++) {
+            if(know[i]) {   // 아는 사람들이 참여한 파티정보 
+                for(int k=0; k<people[i].size(); k++) {
+                    if(!visited[people[i].get(k)]) {
+                        q.add(people[i].get(k));
+                        visited[people[i].get(k)] = true;
+                    }
+                }
+            }
+        }
+        
+        
+        while(!q.isEmpty()) {
+            int a = q.poll();   // 파티번호
+            
+            
+            for(int num : party[a]) {
+                // num 이 참석한 파티정보
+                for(int partyInfo : people[num]) {
+                    if(!visited[partyInfo]) {
+                        q.add(partyInfo);
+                        visited[partyInfo] = true;
+                    }
+                }
+            }
+            
+        }
+        for(int i=0; i<m; i++) {
+            if(!visited[i]) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
     }
 }
