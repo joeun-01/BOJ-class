763fbb630c25dbb959a6e628691baad1ac6e3841
joeun-01 <jjy020121@gmail.com>
2023-05-15 15:16:48 +0900

    23.05.15

---
+++ N1934.java
@@ -0,0 +1,54 @@
+package BOJ;
+
+/* 
+두 자연수 A와 B에 대해서, A의 배수이면서 B의 배수인 자연수를 A와 B의 공배수라고 한다. 
+이런 공배수 중에서 가장 작은 수를 최소공배수라고 한다. 
+예를 들어, 6과 15의 공배수는 30, 60, 90등이 있으며, 최소 공배수는 30이다.
+
+두 자연수 A와 B가 주어졌을 때, A와 B의 최소공배수를 구하는 프로그램을 작성하시오.
+
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 
+둘째 줄부터 T개의 줄에 걸쳐서 A와 B가 주어진다. (1 ≤ A, B ≤ 45,000)
+
+첫째 줄부터 T개의 줄에 A와 B의 최소공배수를 입력받은 순서대로 한 줄에 하나씩 출력한다.
+
+3
+1 45000
+6 10
+13 17
+
+45000
+30
+221
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class N1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder build = new StringBuilder();
+
+        int T = Integer.parseInt(read.readLine());
+
+        for(int i = 0; i < T; i++) {
+            StringTokenizer stoi = new StringTokenizer(read.readLine());
+            int A = Integer.parseInt(stoi.nextToken());
+            int B = Integer.parseInt(stoi.nextToken());
+
+            int lcm = A * B / gcd(Math.max(A, B), Math.min(A, B));
+
+            build.append(lcm + "\n");
+        }
+
+        System.out.println(build);
+    }
+
+    public static int gcd(int A, int B){
+        if(B == 0) return A;
+        else return gcd(B, A % B);
+    }
+}
