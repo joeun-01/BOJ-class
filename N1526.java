45d75d87490a954ac111c41313ed2a1c096b080f
joeun-01 <jjy020121@gmail.com>
2023-05-21 23:24:39 +0900

    23.05.21

---
+++ N1526.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+/*
+은민이는 4와 7을 좋아하고, 나머지 숫자는 싫어한다. 
+금민수는 어떤 수가 4와 7로만 이루어진 수를 말한다.
+
+N이 주어졌을 때, N보다 작거나 같은 금민수 중 가장 큰 것을 출력하는 프로그램을 작성하시오.
+
+첫째 줄에 N이 주어진다. 
+N은 4보다 크거나 같고 1,000,000보다 작거나 같은 자연수이다.
+
+첫째 줄에 N보다 작거나 같은 금민수 중 가장 큰 것을 출력한다.
+
+100
+
+77
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class N1526 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(read.readLine());
+        
+    }
+}
