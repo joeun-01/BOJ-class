f7b7cb0cb08b75631affafacc2ba3aa16eb0dd2c
joeun-01 <jjy020121@gmail.com>
2023-06-13 23:43:18 +0900

    23.06.13

---
+++ N11942.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+/*
+고려대학교는 사랑입니다.
+
+첫째 줄에 “고려대학교”를 출력하세요.
+
+고려대학교
+ */
+
+public class N11942 {
+    public static void main(String[] args) {
+        System.out.println("고려대학교");        
+
+    }
+}
