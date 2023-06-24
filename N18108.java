a7eef627b1d745ac654fbbf1ac080e7cf7a853f3
joeun-01 <jjy020121@gmail.com>
2023-06-25 03:10:54 +0900

    23.06.25

---
+++ N18108.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.io.*;
+
+public class N18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        int year = Integer.parseInt(read.readLine());
+        System.out.println(year - 543);
+    }
+}
