6f8dd41e4a94ce3a53cab8aae3664a28033fc32b
joeun-01 <jjy020121@gmail.com>
2023-10-30 23:48:21 +0900

    23.10.30

---
+++ N1977.java
@@ -0,0 +1,45 @@
+package BOJ;
+
+/*
+M과 N이 주어질 때 M이상 N이하의 자연수 중 완전제곱수인 것을 모두 골라 그 합을 구하고 그 중 최솟값을 찾는 프로그램을 작성하시오. 
+예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 완전제곱수는 64, 81, 100 이렇게 총 3개가 있으므로 그 합은 245가 되고 이 중 최솟값은 64가 된다.
+
+첫째 줄에 M이, 둘째 줄에 N이 주어진다. 
+M과 N은 10000이하의 자연수이며 M은 N보다 같거나 작다.
+
+M이상 N이하의 자연수 중 완전제곱수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다. 
+단, M이상 N이하의 자연수 중 완전제곱수가 없을 경우는 첫째 줄에 -1을 출력한다.
+
+60
+100
+
+245
+64
+ */
+
+import java.io.*;
+
+public class N1977 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(read.readLine());
+        int N = Integer.parseInt(read.readLine());
+
+        int start = (int) Math.round(Math.pow(M, 0.5));
+        int end = (int) Math.pow(N, 0.5);
+
+        if(start > end || start * start < M) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+        
+        long sum = 0;
+        for(int i = start; i <= end; i++) {
+            sum += i * i;
+        }   
+
+        System.out.println(sum);       
+        System.out.println(start * start);
+    }
+}
