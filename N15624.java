859910a39d4b0683549958ef391383e06b2368f6
joeun-01 <jjy020121@gmail.com>
2024-04-09 16:37:23 +0900

    24.04.09

--- N15624.java
+++ N15624.java
@@ -31,8 +31,13 @@ public class N15624 {
 
         int N = Integer.parseInt(read.readLine());
 
-        int[] DP = new int[N + 1];
+        long[] DP = new long[N + 1];
         DP[0] = 0;
+        if(N == 0) {
+            System.out.println(DP[N]);
+            System.exit(0);
+        }
+
         DP[1] = 1;
         for(int i = 2; i <= N; i++) {
             DP[i] = (DP[i - 1] + DP[i - 2]) % 1000000007;
