7fa31f62b7bb55279912bbfd9f1c320618e849b0
joeun-01 <jjy020121@gmail.com>
2024-06-24 00:06:56 +0900

    24.06.24

---
+++ N25206.java
@@ -0,0 +1,88 @@
+package BOJ;
+
+/*
+인하대학교 컴퓨터공학과를 졸업하기 위해서는, 전공평점이 3.3 이상이거나 졸업고사를 통과해야 한다. 
+그런데 아뿔싸, 치훈이는 깜빡하고 졸업고사를 응시하지 않았다는 사실을 깨달았다!
+
+치훈이의 전공평점을 계산해주는 프로그램을 작성해보자.
+전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값이다.
+인하대학교 컴퓨터공학과의 등급에 따른 과목평점은 다음 표와 같다.
+
+A+	4.5
+A0	4.0
+B+	3.5
+B0	3.0
+C+	2.5
+C0	2.0
+D+	1.5
+D0	1.0
+F	0.0
+
+P/F 과목의 경우 등급이 P또는 F로 표시되는데, 등급이 P인 과목은 계산에서 제외해야 한다.
+과연 치훈이는 무사히 졸업할 수 있을까?
+
+20줄에 걸쳐 치훈이가 수강한 전공과목의 과목명, 학점, 등급이 공백으로 구분되어 주어진다.
+
+치훈이의 전공평점을 출력한다.
+정답과의 절대오차 또는 상대오차가 10^{-4} 이하이면 정답으로 인정한다.
+
+ObjectOrientedProgramming1 3.0 A+
+IntroductiontoComputerEngineering 3.0 A+
+ObjectOrientedProgramming2 3.0 A0
+CreativeComputerEngineeringDesign 3.0 A+
+AssemblyLanguage 3.0 A+
+InternetProgramming 3.0 B0
+ApplicationProgramminginJava 3.0 A0
+SystemProgramming 3.0 B0
+OperatingSystem 3.0 B0
+WirelessCommunicationsandNetworking 3.0 C+
+LogicCircuits 3.0 B0
+DataStructure 4.0 A+
+MicroprocessorApplication 3.0 B+
+EmbeddedSoftware 3.0 C0
+ComputerSecurity 3.0 D+
+Database 3.0 C+
+Algorithm 3.0 B0
+CapstoneDesigninCSE 3.0 B+
+CompilerDesign 3.0 D0
+ProblemSolving 4.0 P
+
+3.284483
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class N25206 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("A+", 4.5);
+        map.put("A0", 4.0);
+        map.put("B+", 3.5);
+        map.put("B0", 3.0);
+        map.put("C+", 2.5);
+        map.put("C0", 2.0);
+        map.put("D+", 1.5);
+        map.put("D0", 1.0);
+        map.put("F", 0.0);
+
+        double gradeSum = 0;
+        double scoreSum = 0;
+        for(int i = 0; i < 20; i++) {
+            StringTokenizer stoi = new StringTokenizer(read.readLine());
+
+            String subject = stoi.nextToken();
+            float score = Float.parseFloat(stoi.nextToken());
+            String grade = stoi.nextToken();
+
+            if(grade.equals("P")) continue;
+
+            gradeSum += score * map.get(grade);
+            scoreSum += score;
+        }   
+
+        System.out.println(gradeSum / scoreSum);
+    }
+}
